53f3e7ead7bb12a6cb878a1ffbdcf5cac5a5d797
lichaowei <13503520128@163.com>
2022-11-01 22:07:10 +0800

    fix some issue
    Signed-off-by: lichaowei <13503520128@163.com>

--- binanceApi/wallet/wallet.go
+++ binanceApi/wallet/wallet.go
@@ -3,6 +3,7 @@ package wallet
 import (
 	"QuantitativeFinance/binanceApi/common"
 	"QuantitativeFinance/binanceApi/market"
+	"QuantitativeFinance/setting"
 	"fmt"
 	"github.com/fatih/color"
 	"net/http"
@@ -25,22 +26,22 @@ func SystemStatus(baseUrl string) {
 }
 
 // AccountSnapshot 获取用户当日资产快照 kind: "SPOT","MARGIN","FUTURES"
-func AccountSnapshot(baseUrl string, apiKey string, secretKey string, kind string, days string) {
+func AccountSnapshot(baseUrl string, kind string, days string) {
 	timeStamp := strconv.Itoa(market.ServerTime(baseUrl))
 	param := "type=" + kind + "&limit=" + days + "&recvWindow=5000" + "&timestamp=" + timeStamp
-	signature := common.HmacSha256(secretKey, param)
+	signature := common.HmacSha256(setting.AppSetting.SecreteKey, param)
 	url := baseUrl + "/sapi/v1/accountSnapshot?type=" + kind + "&limit=" + days + "&recvWindow=5000" + "&timestamp=" + timeStamp + "&signature=" + signature
 	// use original way to request an url
-	res := common.HandleRequest("GET", url, apiKey)
+	res := common.HandleRequest("GET", url, setting.AppSetting.ApiKey)
 	color.Green(res)
 }
 
 // GetAll /sapi/v1/capital/config/getall
-func GetAll(baseUrl string, apiKey string, secretKey string) {
+func GetAll(baseUrl string) {
 	timeStamp := strconv.Itoa(market.ServerTime(baseUrl))
 	param := "recvWindow=5000" + "&timestamp=" + timeStamp
-	signature := common.HmacSha256(secretKey, param)
+	signature := common.HmacSha256(setting.AppSetting.SecreteKey, param)
 	url := baseUrl + "/sapi/v1/capital/config/getall?recvWindow=5000" + "&timestamp=" + timeStamp + "&signature=" + signature
-	res := common.HandleRequest("GET", url, apiKey)
+	res := common.HandleRequest("GET", url, setting.AppSetting.ApiKey)
 	color.Green(res)
 }
